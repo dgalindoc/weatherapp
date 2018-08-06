a067d49475f91c2040cf0db395335396cffc409a
david <dgalindoc@gmail.com>
2018-08-06 09:44:05 +0200

    Refactor Folders

--- src/components/WeatherLocation/WeatherData/WeatherTemperature.js
+++ src/components/WeatherLocation/WeatherData/WeatherTemperature.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import WeatherIcons from 'react-weathericons';
-import {CLOUD,SNOW,SUNNY} from './../constants/weathers';
+import {CLOUD,SNOW,SUNNY} from './../../../constants/weathers';
 import PropTypes from 'prop-types';
 
 const stateToIconName = (weatherState) => {

9ad1796e409af65479726097aaf96c427e7093d9
david <dgalindoc@gmail.com>
2018-08-06 11:34:00 +0200

    class Component

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import {SUNNY} from './../../constants/weathers';
 import './style.css';
 
-const WeatherLocation = () => (
-  <div class="weatherLocationCont">
-    <Location city={'Madrid'}/>
-    <WeatherData/>
-  </div>
-);
+const data = {
+  temperature: 20,
+  weatherState: 'SUNNY',
+  humidity: 10,
+  wind: '10 m/s',
+};
+
+class WeatherLocation extends Component {
+  render = () => (
+    <div class="weatherLocationCont">
+      <Location city={'Madrid'}/>
+      <WeatherData data={data}/>
+    </div>
+  );
+}
 
 export default WeatherLocation;
